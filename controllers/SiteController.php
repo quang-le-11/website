0855ecc098a45771b92a3f3ebaf00580399c2ef3
Le Vinh Quang <levinhquangpy@gmail.com>
2022-10-01 22:41:49 +0700

    project_mvc: create mvc #5

---
+++ controllers/SiteController.php
@@ -0,0 +1,30 @@
+<?php
+namespace app\controllers;
+
+use app\core\Controller;
+use app\core\Application;
+use app\core\Request;
+
+class SiteController extends Controller
+{
+    public function home()
+    {
+        $params = [
+            'name' => 'Le Vinh Quang'
+        ];
+        return $this->reder('home', $params);
+    }
+
+    public function contact()
+    {
+        return $this->reder('contact');
+    }
+
+    public function handleContact(Request $request)
+    {
+
+        $body = $request->getBody();
+        var_dump($body);
+        return 'Submit data';
+    }
+}
