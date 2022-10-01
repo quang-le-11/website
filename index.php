b4ba3aa29e86d3303c720a870f651f813cdeb0b7
quang-le-py <levinhquangpy@gmail.com>
2022-10-01 17:37:51 +0700

    project_mvc: create mvc

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
 
-echo "hello world";
+echo 'oke';
+//require_once __DIR__.'/vendor/autoload.php';
+//
+//$app = new \app\core\Application();
+//
+//$app->router->get('/', function (){
+//    return 'Hello World';
+//});
+//
+//$app->router->get('/contact', function (){
+//    return 'Contact';
+//});
+//
+//$app->run();
